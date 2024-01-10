1edbc45bed60723dc29dcea43898395c1c81d1b2
NancyAnthony <tonyuche003@gmail.com>
2024-01-10 13:17:53 -0800

    HI

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""LOOK_UP"""
+
+def lookup(obj):
+    """
+A function that returns list of available
+"""
+return dir (obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""LIST"""
+
+class MyList(list):
+    """class List"""
+
+    def print_sorted(self):
+        """ """A function that prints the list"""
+
+        print(sorted(self))
